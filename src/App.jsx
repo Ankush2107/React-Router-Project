7bb848871333a329596b5f91247214d8e80391aa
Ankush2107 <ankush.kumar210701@gmail.com>
2024-04-23 20:32:31 +0530

    you can see the changes in route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
 
 // pages
 import Home from "./pages/Home/Home"
@@ -7,6 +7,13 @@ import About from "./pages/About/About"
 function App() {
   return (
     <BrowserRouter>
+    <header>
+      <nav>
+        <h1>Find Jobs ?</h1>
+        <Link to='/'>Home</Link>
+        <NavLink to="about">About</NavLink>
+      </nav>
+    </header>
       <main>
         <Routes>
           <Route path="/" element={<Home/>} />
