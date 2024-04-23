1c6a2c8617232e157ae76e9d170115c78c16877a
Ankush2107 <ankush.kumar210701@gmail.com>
2024-04-23 22:08:02 +0530

    newer way to use react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,26 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom"
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom"
 
 // pages
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
 
+// layouts
+import RootLayouts from "./layouts/RootLayouts"
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayouts/>}>
+      <Route path="/" element={<Home/>} />
+      <Route path="about" element={<About/>} />
+    </Route>
+  )
+)
+
 function App() {
   return (
-    <BrowserRouter>
-    <header>
-      <nav>
-        <h1>Find Jobs ?</h1>
-        <NavLink to='/'>Home</NavLink>
-        <NavLink to="about">About</NavLink>
-      </nav>
-    </header>
-      <main>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="about" element={<About/>} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    
+      <RouterProvider router={router} />
+
   )
 }
 
