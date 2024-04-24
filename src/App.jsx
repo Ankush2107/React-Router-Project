72c90b187db6153e11c46caa42df8c77ae3e53f2
Ankush2107 <ankush.kumar210701@gmail.com>
2024-04-24 18:41:40 +0530

    added helplayout in the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
-import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, Route, createRoutesFromElements, RouterProvider } from "react-router-dom";
 
 // pages
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
 
 // layouts
-import RootLayouts from "./layouts/RootLayouts"
+import RootLayouts from "./layouts/RootLayout"
+import HelpLayout from "./layouts/HelpLayout"
+import Faq from "./pages/help/Faq"
+import Contact from "./pages/help/Contact"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayouts/>}>
-      <Route path="/" element={<Home/>} />
+
+      <Route index element={<Home/>} />
       <Route path="about" element={<About/>} />
+
+      <Route path="help" element={<HelpLayout/>} >
+        <Route path="faq" element={<Faq/>} />
+        <Route path="contact" element={<Contact/>} />
+      </Route>
+
     </Route>
   )
 )
 
 function App() {
   return (
-    
       <RouterProvider router={router} />
-
   )
 }
 
